6f6cda929a48652ee753bb25c2f2a42620b96493
unknown <BT_3W2_03@BKTECH.LOCAL>
2022-10-18 09:31:08 -0400

    Initial commit

---
+++ src/RightTriangle.java
@@ -0,0 +1,15 @@
+public class RightTriangle {
+    private double base;
+    private double height;
+
+    public RightTriangle(base, height)
+    {
+        this.base = base;
+        this.height = height;
+    }
+
+    public double hypotenuse()
+    {
+        
+    }
+}
